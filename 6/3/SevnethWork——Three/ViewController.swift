17b54b8f09e98a0b383d02a85d27317f84cb57a4
Ayeea <45812075+Ayeea@users.noreply.github.com>
2018-12-18 15:14:24 +0800

    Add files via upload

---
+++ 6/3/SevnethWork——Three/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SevnethWork——Three
+//
+//  Created by student on 2018/10/27.
+//  Copyright © 2018年 pengyun All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
